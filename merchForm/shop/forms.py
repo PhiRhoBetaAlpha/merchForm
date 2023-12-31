481ba8ae26d31d214e229f107fd1ba8dddfe7476
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-12-30 19:14:39 -0600

    migrated models to add them to admin

--- merchForm/shop/forms.py
+++ merchForm/shop/forms.py
@@ -4,8 +4,5 @@ from . import models
 class MerchForm(forms.Form):
     #this will be the main form for collecting data
     your_name = forms.CharField(label="Your name", max_length=100) #example
-
-    #idk if working. will have to test
-    for shirt in models.Shirts:
-        forms.IntegerField(label = shirt.name+" quaintity", min_value=1, widget=forms.Select())
+    
     #for each merch that exists, there should be an option to see/buy it
