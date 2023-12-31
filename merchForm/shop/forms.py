012b954a16a1c93ab94edae967566ca637de3d7e
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-12-30 19:02:38 -0600

    added forms for main page

---
+++ merchForm/shop/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from . import models
+
+class MerchForm(forms.Form):
+    #this will be the main form for collecting data
+    your_name = forms.CharField(label="Your name", max_length=100) #example
+
+    #for each merch that exists, there should be an option to see/buy it
--- merchForm/shop/views.py
+++ merchForm/shop/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import MerchForm
 
 # Create your views here.
 
 def index(request):
     return render(request, "shop/index.html")
+
+#DUMMY EXAMPLE OF VALIDATION. all variables are examples
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = MerchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = MerchForm()
+
+    return render(request, "name.html", {"form": form})
