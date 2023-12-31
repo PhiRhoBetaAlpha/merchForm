8ff90e6b8c856d1ab98aa66e1f337a6a3b0a69a8
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-12-30 18:43:19 -0600

    updated static files (for images)

--- merchForm/merchForm/urls.py
+++ merchForm/merchForm/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include("shop.urls")), #adds the shops urls to the entire project
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
